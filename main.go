f7e32d314c3a75be253d0727497e4d79eefe3265
WilsonCruzz <wilson860731@gmail.com>
2024-12-11 16:11:42 -0500

    feat. getTodos

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -36,6 +37,8 @@ func main() {
 		log.Fatal(err)
 	}
 
+	defer client.Disconnect(context.Background())
+
 	err = client.Ping(context.Background(), nil)
 	if err != nil {
 		log.Fatal(err)
@@ -48,9 +51,9 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/api/todos", getTodos)
-	app.Post("/api/todos", createTodo)
-	app.Patch("/api/todos", updateTodo)
-	app.Delete("/api/todos", deleteTodo)
+	// app.Post("/api/todos", createTodo)
+	// app.Patch("/api/todos", updateTodo)
+	// app.Delete("/api/todos", deleteTodo)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -60,7 +63,38 @@ func main() {
 	log.Fatal(app.Listen("0.0.0.0:" + port))
 }
 
-func getTodos(c *fiber.Ctx) error {}
+func getTodos(c *fiber.Ctx) error {
+	// Declare a slice to hold the list of todos
+	var todos []Todo
+
+	// Perform a query to find all documents in the collection (no filter)
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		// If an error occurs during the query, return the error
+		return err
+	}
+
+	// Ensure the cursor is closed when the function finishes
+	defer cursor.Close(context.Background())
+
+	// Iterate over the documents returned by the query
+	for cursor.Next(context.Background()) {
+		// Declare a variable to hold each todo item
+		var todo Todo
+
+		// Decode the current document into the todo variable
+		if err := cursor.Decode(&todo); err != nil {
+			// If decoding fails, return the error
+			return err
+		}
+
+		// Append the decoded todo to the todos slice
+		todos = append(todos, todo)
+	}
+
+	// Return the todos list as a JSON response
+	return c.JSON(todos)
+}
 
 // func createTodo(c *fiber.Ctx) error {}
 // func updateTodo(c *fiber.Ctx) error {}
